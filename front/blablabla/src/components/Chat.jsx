5131a36aa7097482fb005a7f2eea35ffa31235f0
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-05-12 20:40:34 +0400

    create useEffect for gettring searchparams

--- front/blablabla/src/components/Chat.jsx
+++ front/blablabla/src/components/Chat.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import io from "socket.io-client";
+import { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+
+const socket = io.connect("http://localhost:5000");
 
 const Chat = () => {
+  const { search } = useLocation();
+  const [ params, setParams ] = useState(null);
+
+  console.log("location", location);
+  useEffect(() => {
+    const params = new URLSearchParams(search);
+    const searchParams = Object.fromEntries(params.entries());
+    setParams(searchParams)
+    socket.emit('join', searchParams);
+  }, [search]);
+
   return (
     <div>Chat</div>
   )
