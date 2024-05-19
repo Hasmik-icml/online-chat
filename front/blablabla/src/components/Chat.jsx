74d79f64fe8d68c72733b259db85e69308a308d2
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-05-19 10:15:28 +0400

    Add socket.on

--- front/blablabla/src/components/Chat.jsx
+++ front/blablabla/src/components/Chat.jsx
@@ -7,15 +7,23 @@ const socket = io.connect("http://localhost:5000");
 
 const Chat = () => {
   const { search } = useLocation();
-  const [ params, setParams ] = useState(null);
+  const [params, setParams] = useState(null);
+  const [chat, setChat] = useState([]);
 
   console.log("location", location);
+
   useEffect(() => {
     const params = new URLSearchParams(search);
     const searchParams = Object.fromEntries(params.entries());
     setParams(searchParams)
     socket.emit('join', searchParams);
   }, [search]);
+
+  useEffect(() => {
+    socket.on('message', ({ data }) => {
+      setChat((_chat) => { [...chat, data] })
+    });
+  }, []);
 
   return (
     <div>Chat</div>
